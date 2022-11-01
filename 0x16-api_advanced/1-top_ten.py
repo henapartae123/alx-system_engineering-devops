abd51a8cbeed0b0ef85d92c86e169b34c55ae686
Henok Shewakena <henyvibe@gmail.com>
2022-11-01 12:09:02 +0300

    api adv

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Working with the Reddit API.
+"""
+import requests
+
+
+BASE_URL = 'https://www.reddit.com'
+"""Reddit's base API URL."""
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts listed for a given subreddit"""
+
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    limit = 10
+    sort = 'top'
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL, subreddit, sort, limit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        for listing in res.json()['data']['children'][0:10]:
+            print(listing['data']['title'])
+    else:
+        print(None)
